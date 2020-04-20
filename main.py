dcb83c0637d6b81e58db87ee2c82e08c24897f7f
brandonyuh <brandonyuh@gmail.com>
2020-04-20 15:32:44 -0700

    Added thread to check clipboard for changes

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import pyperclip
+import threading
+import time
+
+check_delay = 1
+
+
 def main():
-    print("hello clipboard")
+    tread = threading.Thread(target=periodic_check)
+    tread.start()
+
+
+def periodic_check():
+    clipboard_value = ""
+    while True:
+        clipboard_value = check_changed(clipboard_value)
+
+        time.sleep(check_delay)
+
+
+def check_changed(clipboard_value):
+    new_clipboard_value = pyperclip.paste()
+    if clipboard_value != new_clipboard_value:
+        clipboard_value = new_clipboard_value
+        # TODO record value
+
+    return clipboard_value
 
 
 if __name__ == "__main__":
